48b2c6ffdc502dbd49e6c1686ea526a0798f3c13
Lilouoz <honoreaurelie@hotmail.com>
2017-04-11 17:26:37 +0200

    contenu_cut
    réduction du texte a 100 caractères

--- projet3/app/frontend/classe/quatre_ad_billets.php
+++ projet3/app/frontend/classe/quatre_ad_billets.php
@@ -1,6 +1,6 @@
 <?php 
 // On récupère les 5 derniers billets
-$req = $bdd->query('SELECT id, image, titre, contenu, auteur, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 1, 4');
+$req = $bdd->query('SELECT id, image, titre, IF(CHAR_LENGTH(contenu) > 100, CONCAT(LEFT(contenu, 100) ), contenu) AS contenu_cut, auteur, auteur, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 1, 4');
 
 //on obtient le nombre de commentaire par billet
 
@@ -21,7 +21,7 @@ while ($donnees = $req->fetch())
 				<p>
 					<?php
     // On affiche le contenu du billet
-    echo nl2br(htmlspecialchars($donnees['contenu']));
+    echo nl2br(htmlspecialchars($donnees['contenu_cut']));
     ?>
 				</p>
 				<label>
